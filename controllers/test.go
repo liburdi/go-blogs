7cdd14a54e806f394fa22ab6e09bcad08b8b2e04
chencf <KenRitChie@163.com>
2020-03-05 18:48:35 +0800

    mini online config

--- controllers/test.go
+++ controllers/test.go
@@ -69,7 +69,7 @@ func Note(w http.ResponseWriter, r *http.Request) {
 }
 func IsOnline(w http.ResponseWriter,r *http.Request){
 	data:=make(map[string]interface{})
-	data["is_online"]=0
+	data["is_online"]=1
 	api := init_com.ApiRestful{
 		Code:    200,
 		Message: "Success",
