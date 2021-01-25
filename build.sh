1a3846e13e18a76154e0a4c5e955c839caf8fcbb
陈朝飞 <chenzhaofei@chenchaofeideMacBook-Pro.local>
2021-01-25 19:49:15 +0800

    自动化docker构建优化

--- build.sh
+++ build.sh
@@ -2,7 +2,13 @@
 docker stop go_blog
 docker rm $(docker ps -a -q)
 timeNow=$(date "+%Y%m%d%H%M%S")
+
 git pull
-go build -o blog
+
+if [ ! -d static ]; then
+        mkdir static
+fi
+
+go build -o golangschool
 docker build -t golangschool:$timeNow .
 docker run -t -i -d -p 127.0.0.1:8080:8080  --name go_blog golangschool:$timeNow
