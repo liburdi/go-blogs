c321757fc016d78da3d22a247b0126e7c53abedc
陈朝飞 <chenzhaofei@chenchaofeideMacBook-Pro.local>
2021-01-25 20:03:37 +0800

    build.sh opt

--- build.sh
+++ build.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
-if [ $1 == 'stop' ]; then
+
+if [ $1 = "stop" ]; then
   docker stop go_blog
 fi
 
-if [ $1 == 'restart' ]; then
+if [ $1 =  "restart" ]; then
   docker stop go_blog
   ./build.sh start
-if
+fi
 
-if [ $1 == 'start' ]; then
+if [ $1 =  "start" ]; then
   docker rm $(docker ps -a -q)
   timeNow=$(date "+%Y%m%d%H%M%S")
 
   git pull
-
   if [ ! -d static ]; then
           mkdir static
   fi
-
   go build -o golangschool
   docker build -t golangschool:$timeNow .
   docker run -t -i -d -p 127.0.0.1:8080:8080  --name go_blog golangschool:$timeNow
-if
 
+fi
